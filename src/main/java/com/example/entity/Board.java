30d09e26c35ce69f10b3fc5b5c6c9140abb62eed
egoing <egoing@gmail.com>
2023-04-13 15:50:46 +0900

    다른 컴퓨터: 1

---
+++ src/main/java/com/example/entity/Board.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
