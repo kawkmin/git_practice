5a038bbcff9e9ea25a47622973a76e5e33495d78
kawksung <alstjd5479@naver.com>
2023-04-13 14:20:59 +0900

    develop 브런치:2

---
+++ src/main/java/com/example/entity/Address.java
@@ -0,0 +1,18 @@
+package com.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Address {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+}
